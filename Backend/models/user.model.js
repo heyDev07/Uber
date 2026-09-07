728b6b627f2059005744968b2db17cefe2455a44
heyDev07 <295devraj@gmail.com>
2026-09-07 16:47:33 +0530

    The Driver/user registration is complete

--- Backend/models/user.model.js
+++ Backend/models/user.model.js
@@ -2,50 +2,43 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
-
 const userSchema = new mongoose.Schema({
     fullname: {
         firstname: {
             type: String,
             required: true,
-            minlength: [ 3, 'First name must be at least 3 characters long' ],
+            minlength: [3, 'First name must be at least 3 characters long']
         },
         lastname: {
             type: String,
-            minlength: [ 3, 'Last name must be at least 3 characters long' ],
+            minlength: [3, 'Last name must be at least 3 characters long']
         }
     },
     email: {
         type: String,
         required: true,
         unique: true,
-        minlength: [ 5, 'Email must be at least 5 characters long' ],
+        minlength: [5, 'Email must be at least 5 characters long']
     },
     password: {
         type: String,
         required: true,
-        select: false,
-    },
-    //for getting real time location
-    socketId: {
-        type: String,
-    },
-})
+        select: false
+    }
+});
 
 userSchema.methods.generateAuthToken = function () {
-    const token = jwt.sign({ _id: this._id }, process.env.JWT_SECRET);
-    return token;
-}
-
-userSchema.methods.comparePassword = async function (password) {
-    return await bcrypt.compare(password, this.password);
-}
+    return jwt.sign({ _id: this._id }, process.env.JWT_SECRET);
+};
 
 userSchema.statics.hashPassword = async function (password) {
     return await bcrypt.hash(password, 10);
-}
+};
+
+userSchema.methods.comparePassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
+};
 
 const userModel = mongoose.model('user', userSchema);
-
 
 module.exports = userModel;
